5d42507768ce4a356bb44a3eaa8bbec4f5c0fc29
izalmfrds <67822136+izalmfrds@users.noreply.github.com>
2024-10-04 21:31:26 +0700

    update best project

--- src/components/bestProject.tsx
+++ src/components/bestProject.tsx
@@ -4,17 +4,15 @@ import Image from "next/image";
 import { Button } from "@nextui-org/button";
 import Best1 from "@/Assets/Image/best1.png";
 import Best1_2 from "@/Assets/Image/best1.2.png";
+import Best1_3 from "@/Assets/Image/best1_3.png";
+import Best1_4 from "@/Assets/Image/best1_4.png";
 import Best2 from "@/Assets/Image/best2.png";
 import Best3 from "@/Assets/Image/best3.png";
 import LeftIcon from "@/Assets//Icon/Arrow";
 
-export default function bestProject() {
+export default function BestProject() {
   useEffect(() => {
     const stickySections = document.querySelectorAll(".sticky_wrap");
-
-    stickySections.forEach((section) => {
-      transform(section as HTMLElement);
-    });
 
     const handleScroll = () => {
       stickySections.forEach((section) => {
@@ -22,7 +20,7 @@ export default function bestProject() {
       });
     };
 
-    function transform(section: HTMLElement) {
+    const transform = (section: HTMLElement) => {
       const offsetTop = section.parentElement?.offsetTop || 0;
       const scrollSection = section.querySelector(
         ".horizontal_scroll"
@@ -31,20 +29,10 @@ export default function bestProject() {
       if (scrollSection) {
         let percentage =
           ((window.scrollY - offsetTop) / window.innerHeight) * 100;
-        percentage = Math.max(0, Math.min(300, percentage)); // Batasi antara 0 dan 300
+        percentage = Math.max(0, Math.min(300, percentage));
         scrollSection.style.transform = `translate3d(${-percentage}vw, 0, 0)`;
       }
-    }
-
-    // Function declaration mendukung hoisting, sehingga bisa dipanggil sebelum deklarasi
-    stickySections.forEach((section) => {
-      transform(section as HTMLElement);
-    });
-
-    // Kemudian gunakan fungsi transform
-    stickySections.forEach((section) => {
-      transform(section as HTMLElement); // Pemanggilan di sini
-    });
+    };
 
     window.addEventListener("scroll", handleScroll);
 
@@ -55,22 +43,13 @@ export default function bestProject() {
 
   return (
     <main className="w-full h-full">
-      {/* <section className="h-screen">
-        <div className="container mx-auto py-20">
-          <h1 className="text-4xl font-bold ">Digital Telkom Schools</h1>
-          <p className="mt-4">
-            Dummy text dummy text dummy text dummy text dummy text.
-          </p>
-        </div>
-      </section> */}
-
       <div className="scroll_container h-[400vh]">
         <div className="sticky_wrap sticky top-0 h-screen overflow-hidden">
           <div className="horizontal_scroll absolute top-0 h-full w-[400vw] flex justify-between">
             <div className="scroll_contents w-screen h-full bg-[#DA4641] flex items-start">
-              <div className="w-full sm:w-full md:w-1/3 lg:w-full rounded-md ">
-                <Image src={Best1} alt="best1" className=" " />
-                <div className="absolute top-[70%] left-0 transform -translate-y-[45%] flex flex-row   gap-6 pl-4">
+              <div className="w-full sm:w-full md:w-1/3 lg:w-full rounded-md relative">
+                <Image src={Best1} alt="best1" className="w-full h-auto" />
+                <div className="absolute top-[70%] left-[3.5%] transform -translate-y-[45%] flex flex-row gap-6 pl-4">
                   <Button
                     variant="light"
                     className="text-white hover:text-blue-500"
@@ -86,12 +65,54 @@ export default function bestProject() {
                     View Website
                   </Button>
                 </div>
-              </div>{" "}
+              </div>
             </div>
             <div className="scroll_contents w-screen h-full bg-[#DA4641]">
-              <div className="w-full sm:w-full md:w-1/3 lg:w-full rounded-md ">
-                <Image src={Best1_2} alt="best1" className=" " />
-                <div className="absolute top-[70%] left-0 transform -translate-y-[45%] flex flex-row   gap-6 pl-4">
+              <div className="w-full sm:w-full md:w-1/3 lg:w-full rounded-md relative">
+                <Image src={Best1_2} alt="best1_2" className="w-full h-auto" />
+                <div className="absolute top-[70%] left-[3.5%] transform -translate-y-[45%] flex flex-row gap-6 pl-4">
+                  <Button
+                    variant="light"
+                    className="text-white hover:text-blue-500"
+                    startContent={<LeftIcon />}
+                  >
+                    View Study Case
+                  </Button>
+                  <Button
+                    variant="light"
+                    className="text-white hover:text-blue-500"
+                    startContent={<LeftIcon />}
+                  >
+                    View Website
+                  </Button>
+                </div>
+              </div>
+            </div>
+            <div className="scroll_contents w-screen h-full bg-[#DA4641]">
+              <div className="w-full sm:w-full md:w-1/3 lg:w-full rounded-md relative">
+                <Image src={Best1_3} alt="best1_3" className="w-full h-auto" />
+                <div className="absolute top-[70%] left-[3.5%] transform -translate-y-[45%] flex flex-row gap-6 pl-4">
+                  <Button
+                    variant="light"
+                    className="text-white hover:text-blue-500"
+                    startContent={<LeftIcon />}
+                  >
+                    View Study Case
+                  </Button>
+                  <Button
+                    variant="light"
+                    className="text-white hover:text-blue-500"
+                    startContent={<LeftIcon />}
+                  >
+                    View Website
+                  </Button>
+                </div>
+              </div>
+            </div>
+            <div className="scroll_contents w-screen h-full bg-[#DA4641]">
+              <div className="w-full sm:w-full md:w-1/3 lg:w-full rounded-md relative">
+                <Image src={Best1_4} alt="best1_4" className="w-full h-auto" />
+                <div className="absolute top-[60%] left-[2%] transform -translate-y-[45%] flex flex-row gap-6 pl-4">
                   <Button
                     variant="light"
                     className="text-white hover:text-blue-500"
@@ -108,21 +129,30 @@ export default function bestProject() {
                   </Button>
                 </div>
               </div>{" "}
-            </div>
-            <div className="scroll_contents w-screen h-full bg-green-500"></div>
-            <div className="scroll_contents w-screen h-full bg-blue-500 flex items-end justify-end">
-              <h2 className="text-white text-4xl mr-5 mb-5">Goodbye</h2>
             </div>
           </div>
         </div>
       </div>
 
-      <section className="h-screen bg-gray-100">
-        <div className="container mx-auto py-20">
-          <h1 className="text-4xl font-bold">The End</h1>
-          <p className="mt-4">
-            Dummy text dummy text dummy text dummy text dummy text.
-          </p>
+      <section className="h-screen bg-[#FF9F66]">
+        <div className="w-full sm:w-full md:w-1/3 lg:w-full rounded-md relative">
+          <Image src={Best2} alt="best2" className="w-full h-auto" />
+          <div className="absolute top-[70%] left-[3.5%] transform -translate-y-[45%] flex flex-row gap-6 pl-4">
+            <Button
+              variant="light"
+              className="text-white hover:text-blue-500"
+              startContent={<LeftIcon />}
+            >
+              View Study Case
+            </Button>
+            <Button
+              variant="light"
+              className="text-white hover:text-blue-500"
+              startContent={<LeftIcon />}
+            >
+              View Website
+            </Button>
+          </div>
         </div>
       </section>
     </main>
---
+++ src/components/bestProjectMob.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { useEffect } from "react";
+import Image from "next/image";
+import { Button } from "@nextui-org/button";
+import Best1 from "@/Assets/Image/best1.png";
+import Best1_2 from "@/Assets/Image/best1.2.png";
+import Best2 from "@/Assets/Image/best2.png";
+import Best3 from "@/Assets/Image/best3.png";
+import LeftIcon from "@/Assets//Icon/Arrow";
+
+export default function BestProjectWeb() {
+  return <main className="w-full h-full">sds</main>;
+}
